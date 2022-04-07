5d9005c204d40e206e58c9b6726b92260d8c1700
Jan <Jan.Ballhorn@tacke-marketing.de>
2022-04-07 09:36:10 +0200

    neu

---
+++ src/ContaoHelloWorldBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of [package name].
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Ballhorn\ContaoHelloWorldBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoHelloWorldBundle extends Bundle
+{
+}
